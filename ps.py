2ea295c26fda06f9221f2d2884209c27e2485d50
Josh Parker <jparker@sherpas.ai>
2022-03-02 15:17:08 -0700

    pylint

--- ps.py
+++ ps.py
@@ -1,12 +1,13 @@
+"""simple pyglet demo"""
 import pyglet
 
-clickCount=0
+CLICK_COUNT=0
 
 window = pyglet.window.Window(480,360)
 
 treasure = pyglet.sprite.Sprite(img=pyglet.resource.image('riches.png'))
 
-clicks = pyglet.text.Label(text="Clicks: " + str(clickCount), x=5, y=345)
+clicks = pyglet.text.Label(text="Clicks: " + str(CLICK_COUNT), x=5, y=345)
 
 sound = pyglet.resource.media('cash.wav', streaming=False)
 
@@ -14,32 +15,35 @@ spinCoin = pyglet.image.ImageGrid(pyglet.image.load('coin.png'), 6, 1)
 coin = pyglet.image.Animation.from_image_sequence(spinCoin,1/24.0)
 coins = []
 
-def animateCoins(dt):
- for i in range(len(coins)):
-  if coins[i].opacity <= 0:
-   del coins[i]
-   return
-  coins[i].y += 1
-  coins[i].opacity -= 4
+"""coin rises and fades"""
+def animate_coins():
+    for i in range(len(coins)):
+        if coins[i].opacity <= 0:
+            del coins[i]
+            return
+        coins[i].y += 1
+        coins[i].opacity -= 4
 
+"""what to show in the window"""
 @window.event
 def on_draw():
- treasure.draw()
- clicks.draw()
- for coin in coins:
-  coin.draw()
+    treasure.draw()
+    clicks.draw()
+    for coin in coins:
+        coin.draw()
 
+"""count the clicks"""
 @window.event
-def on_mouse_press(mouseX,mouseY,button,modifiers):
- global clickCount
- clickCount += 1
- clicks.text = "Clicks: " + str(clickCount)
- sound.play()
- coins.append(pyglet.sprite.Sprite(
-  img=coin,
-  x=mouseX, y=mouseY))
+def on_mouse_press(mouse_x,mouse_y):
+    global CLICK_COUNT
+    CLICK_COUNT += 1
+    clicks.text = "Clicks: " + str(CLICK_COUNT)
+    sound.play()
+    coins.append(pyglet.sprite.Sprite(
+        img=coin,
+        x=mouse_x, y=mouse_y))
 
-pyglet.clock.schedule_interval(animateCoins, 1/120.0)
+pyglet.clock.schedule_interval(animate_coins, 1/120.0)
 
 music = pyglet.media.Player()
 music.queue(pyglet.resource.media('news.wav'))
